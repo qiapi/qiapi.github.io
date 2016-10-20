b3b662b358e117e84198975b9ad62f266eef99e6
cjh <1296668784@qq.com>
2016-10-20 16:47:59 +0800

    router

--- my-app.js
+++ my-app.js
@@ -5,6 +5,7 @@ var mainView = myApp.addView('.view-main');
 myApp.onPageInit('about',function(page){
 	myApp.alert("about 页面加载完毕");
 })
+myApp.router.loadPage("<h1>haha</h1>");
 
 /*$$(document).on('pageInit',function(e){
 	var page = e.detail.page;
