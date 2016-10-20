4d31a55cddac281ecf51e79f0f61906b29caf99e
cjh <1296668784@qq.com>
2016-10-20 16:10:44 +0800

    add

---
+++ my-app.js
@@ -0,0 +1,14 @@
+var myApp = new Framework7();
+var $$ = Dom7;
+var mainView = myApp.addView('.view-main');
+
+myApp.onPageInit('about',function(page){
+	myApp.alert("about 页面加载完毕");
+})
+
+/*$$(document).on('pageInit',function(e){
+	var page = e.detail.page;
+	if(page.name === 'about') {
+		myApp.alert("about页面加载完毕2");
+	}
+})*/
