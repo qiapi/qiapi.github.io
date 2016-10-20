9807add83d2b3c6f383165c1cb1b66a3dcf4bbfb
cjh <1296668784@qq.com>
2016-10-20 17:28:34 +0800

    inline pages

--- my-app.js
+++ my-app.js
@@ -1,11 +1,13 @@
 var myApp = new Framework7();
 var $$ = Dom7;
-var mainView = myApp.addView('.view-main');
+var mainView = myApp.addView('.view-main',{
+	domCache:true
+});
 
 myApp.onPageInit('about',function(page){
 	myApp.alert("about 页面加载完毕");
 })
-var newContent = '<div class="page" data-page="my-page">' +
+/*var newContent = '<div class="page" data-page="my-page">' +
                         '<div class="page-content">' +
                           '<p>Here comes new page</p>' +
                           '<a href="index.html" class="back"> Go back to home page </a>	'+
@@ -15,7 +17,7 @@ mainView.router.loadContent(newContent);
 mainView.router.load({
 	content:newContent,
 	animatePages:false
-});
+});*/
 /*$$(document).on('pageInit',function(e){
 	var page = e.detail.page;
 	if(page.name === 'about') {
