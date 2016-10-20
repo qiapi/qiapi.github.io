e6a3d9c72e269e4e499e5c24f90fc31b1a1fac34
cjh <1296668784@qq.com>
2016-10-20 17:15:09 +0800

    dynamic pages

--- my-app.js
+++ my-app.js
@@ -5,7 +5,16 @@ var mainView = myApp.addView('.view-main');
 myApp.onPageInit('about',function(page){
 	myApp.alert("about 页面加载完毕");
 })
-mainView.router.loadPage('about.html');
+var newContent = '<div class="page" data-page="my-page">' +
+                        '<div class="page-content">' +
+                          '<p>Here comes new page</p>' +
+                        '</div>' +
+                      '</div>';
+mainView.router.loadContent(newContent);
+mainView.router.load({
+	content:newContent,
+	animatePages:false
+});
 /*$$(document).on('pageInit',function(e){
 	var page = e.detail.page;
 	if(page.name === 'about') {
