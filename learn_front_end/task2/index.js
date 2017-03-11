60fe96e038f477a4adea41814487f17787a811f4
cjh <1296668784@qq.com>
2017-03-11 14:17:50 +0800

    remind function

--- learn_front_end/task2/index.js
+++ learn_front_end/task2/index.js
@@ -15,7 +15,7 @@ function menu() {
 }
 
 function showMenu() {
-	$("button").click(function() {
+	$(".menuBtn").click(function() {
 		var content = ["remindContent","homeworkContent","noneContent","subjectContent"];
 		for(var j = 0;j<content.length;j++) {
 			$("."+content[j]).fadeOut();
@@ -26,15 +26,49 @@ function showMenu() {
 
 /*获取通知消息*/
 function getRemind() {
+	console.log("on");
 	$.getJSON("remindData.json",function(data) {
 		var content = "";
-		$.each(data,function(item) {
-			content = content + "<p>"+item.remindContent+"</p>";
+		console.log("h");
+		$.each(data,function(itemIndex,item) {
+			console.log(item);
+			content = content + "<div class='block'>"+
+				"<p class='remindTitle'>"+item.about+"</p>"+
+				"<p class='remindinform'>"+item.remindContent+"</p>"+
+				"<span class='remindDate'>"+item.date+"</span>"+
+			"</div>";
 		});
+		console.log(data);
 		$("#remindContent").html(content);
 	});
+	/*var request = new XMLHttpRequest();
+	request.open("GET","remindData.json");
+	request.send();
+	request.onreadystateChange=function() {
+		if(request.readyState===4) {
+			if(request.status===200) {
+				console.log(request.responseText);
+			}
+			else {
+				alert("error"+request.status);
+			}
+		}
+	}*/
+	/*$.ajax({
+		type:"GET",
+		url:"remindData.json",
+		dataType:"json",
+		success: function(data) {
+			console.log(data);
+		},
+		error:function(jqxhr) {
+			alert(jqxhr.status)
+		}
+	});*/
 }
 $(document).ready(function() {
+	console.log("1");
+	console.log("2");
 	getRemind();
 	menu();
 	showMenu();
